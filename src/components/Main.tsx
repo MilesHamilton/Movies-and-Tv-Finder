a6091e9c549a3a1e89fa3cfee746423a274581ec
Miles <mham7714@gmail.com>
2020-12-11 14:01:18 -0500

    added carousal

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,30 @@
 import MovieContent from "./MovieContent"
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useRef } from "react"
 import TvContent from "./TvContent"
-import { Layout, Row, Col, Input, AutoComplete } from "antd"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+import {
+	Layout,
+	Row,
+	Col,
+	Input,
+	AutoComplete,
+	Carousel,
+	InputNumber,
+} from "antd"
 import "./main.css"
 
 const App = () => {
 	const [movie, setMovie] = useState<any[]>([])
-	const [tv, setTv] = useState("")
+	const [tv, setTv] = useState<any[]>([])
 	const [randomMovie, setRandomMovie] = useState("")
 	const [randomTv, setRandomTv] = useState("")
 	const [details, setDetails] = useState<any[]>([])
-	console.log(details)
+	const [movSlide, setMovSlide] = useState<number>(0)
+	console.log(tv)
+
+	const { Header, Footer, Sider, Content } = Layout
 
 	useEffect(() => {
 		handleMovieData()
@@ -19,8 +33,7 @@ const App = () => {
 		handleRandomTv()
 	}, [])
 
-	const { Header, Footer, Sider, Content } = Layout
-
+	// API calls
 	const handleTvData = async () => {
 		let random = Math.floor(Math.random() * (2 - 1 + 1)) + 1
 		let tvUrl = `https://api.themoviedb.org/3/discover/tv?api_key=d99ca085dcabfdf79d02b94e61ac56c4&language=en-US&timezone=America%2FNew_York&vote_average.gte=0&include_null_first_air_dates=false&page=1`
@@ -44,7 +57,7 @@ const App = () => {
 		let moviesUrl = `https://api.themoviedb.org/3/discover/movie?api_key=d99ca085dcabfdf79d02b94e61ac56c4&language=en-US&include_adult=false&include_video=false&vote_average.gte=0&page=${random}`
 		const res = await fetch(moviesUrl)
 		const data = await res.json()
-		console.log(data)
+		// console.log(data)
 		setMovie(data.results)
 	}
 
@@ -56,35 +69,68 @@ const App = () => {
 		// console.log(data)
 		setRandomMovie(data)
 	}
-
+	// Creates movie cards
 	const showMovies = () => {
 		return (
 			movie &&
 			movie.map((mv) => {
 				return (
-					<Col className="movie_card_container">
-						<div
-							className="img"
-							onClick={() => {
-								setDetails([mv])
-							}}
-						>
-							<img
-								src={"https://image.tmdb.org/t/p/w200/" + mv.poster_path}
-								alt={mv.poster_path}
-							/>
-						</div>
+					<div>
+						<Col className="movie_card_container">
+							<div
+								className="img"
+								onClick={() => {
+									setDetails([mv])
+								}}
+							>
+								<img
+									src={"https://image.tmdb.org/t/p/w200/" + mv.poster_path}
+									alt={mv.poster_path}
+								/>
+							</div>
 
-						<h1 className="title">{mv.title}</h1>
+							<h1 className="title">{mv.title}</h1>
 
-						<h2 className="vote_average">{mv.vote_average}/10</h2>
-						<p className="release_date">{mv.release_date}</p>
-					</Col>
+							<h2 className="vote_average">{mv.vote_average}/10</h2>
+							<p className="release_date">{mv.release_date}</p>
+						</Col>
+					</div>
+				)
+			})
+		)
+	}
+	//  creates TV cards
+	const showTv = () => {
+		return (
+			tv &&
+			tv.map((tv) => {
+				return (
+					<div>
+						<Col className="movie_card_container">
+							<div
+								className="img"
+								onClick={() => {
+									setDetails([tv])
+								}}
+							>
+								<img
+									src={"https://image.tmdb.org/t/p/w200/" + tv.poster_path}
+									alt={tv.poster_path}
+								/>
+							</div>
+
+							<h1 className="title">{tv.original_name}</h1>
+
+							<h2 className="vote_average">{tv.vote_average}/10</h2>
+							<p className="release_date">{tv.first_air_date}</p>
+						</Col>
+					</div>
 				)
 			})
 		)
 	}
 
+	// Shows Movie or TV detials
 	const showDetails = () => {
 		return (
 			<div>
@@ -103,6 +149,42 @@ const App = () => {
 		)
 	}
 
+	const carousalSettings = {
+		slidesToShow: 8,
+		slidesToScroll: 4,
+		autoplay: true,
+		pauseOnHover: true,
+		draggable: true,
+		autoplaySpeed: 5000,
+
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					initialSlide: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	}
+
 	return (
 		<div className="App">
 			<Layout>
@@ -117,26 +199,14 @@ const App = () => {
 				</Header>
 				<div className="main_content">
 					<Content>
-						<Row className="movie_card_wrapper">{showMovies()}</Row>
-						{/* <Tabs
-					active={this.state.active}
-					onChange={(active) => this.setState({ active })}
-				>
-					<button className="movieTab" key="movieTab">
-						Trending Movies
-					</button>
-					<button className="randomMovieTab" key="randomMovieTab">
-						Random Movies
-					</button>
-					<button className="tvTab" key="tvTab">
-						Trending Series
-					</button>
-					<button className="randomTvTab" key="randomTvTab">
-						Random Series
-					</button>
-				</Tabs>
-				<div>{content[this.state.active]}</div> */}
+						<Row>
+							<Carousel {...carousalSettings}>{showMovies()}</Carousel>
+						</Row>
+						<Row>
+							<Carousel {...carousalSettings}>{showTv()}</Carousel>
+						</Row>
 					</Content>
+
 					<Sider>{details[0] == null ? null : showDetails()}</Sider>
 				</div>
 
